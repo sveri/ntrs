c8469be6c2e67f00c4c28d48ac43150f41535704
sveri <sveri80@gmail.com>
2015-06-28 21:52:09 +0200

    added added_at

--- src/clj/de/sveri/ntrs/db/toread.clj
+++ src/clj/de/sveri/ntrs/db/toread.clj
@@ -1,6 +1,6 @@
 (ns de.sveri.ntrs.db.toread
   (:require [korma.core :refer [defentity many-to-many select where limit insert values update delete set-fields]])
-  (:import (java.util UUID)))
+  (:import (java.util UUID Date)))
 
 (declare TOREAD)
 
@@ -13,8 +13,9 @@
 (defn get-toread-by-uuid [uuid] (first (select TOREAD (where {:UUID uuid}) (limit 1))))
 
 (defn create-toread [title link description done tags author user]
-  (insert TOREAD (values {:TITLE title :LINK link :DESCRIPTION description :DONE done :TAGS tags :AUTHOR author
-                          :UUID (str (UUID/randomUUID)) :USER user})))
+  (insert TOREAD (values {:TITLE    title :LINK link :DESCRIPTION description :DONE done :TAGS tags :AUTHOR author
+                          :UUID     (str (UUID/randomUUID)) :USER user
+                          :ADDED_AT (Date.)})))
 
 (defn update-toread [uuid fields]
   (update TOREAD (set-fields fields) (where {:UUID uuid})))
